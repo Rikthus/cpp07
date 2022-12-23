2fe57005553d80db44ff313f506bf4a62819cd2e
maxperei <maxperei@student.42lyon.fr>
2022-12-23 03:53:05 +0100

    Done ex00

---
+++ ex01/include/iter.hpp
@@ -0,0 +1,20 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+# include <string>
+
+template<typename T>
+void	print_current(T const & elem)
+{
+	std::cout << elem << std::endl;
+}
+
+template< typename T >
+void	iter(T *tab, size_t tabSize, void (*fct)(T const &))
+{
+	for (size_t i = 0; i < tabSize; i++)
+		fct(tab[i]);
+}
+
+#endif
---
+++ ex01/source/main.cpp
@@ -0,0 +1,2 @@
+#include "../include/iter.hpp"
+
